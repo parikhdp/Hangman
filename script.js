de2fb4b1f71613936862215b53073374807cf3b8
Dhriti <dhriti.parikh2603@gmail.com>
2024-04-15 22:36:47 +0530

    Adding comments to code

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const keyboardDiv = document.querySelector(".keyboard");
 const wordDisplay = document.querySelector(".word-display");
 
+let currentWord;
+//get a random word displayed on screen
 const getRandomWord = () =>
 {
     const {word,hint} = wordList[Math.floor(Math.random()* wordList.length)];
     console.log(word);
+    currentWord= word;
     document.querySelector(".hint-text b").innerText = hint;
     wordDisplay.innerHTML = word.split("").map(()=>'<li class="letter"></li>').join("");
 }
+//checking if clicked letter exists in the word
 const initGame = (button, clickedLetter) =>
 {
-    console.log(clickedLetter);
+    if(currentWord.includes(clickedLetter))
+    {
+        console.log(clickedLetter, " exists in the word");
+    }
+    else
+    {
+        console.log(clickedLetter, " doesn't exist in the word");
+    }
 }
 
+//creating the keyboard
 for (let i = 97; i < 122; i++)
 {
     const button = document.createElement("button");
